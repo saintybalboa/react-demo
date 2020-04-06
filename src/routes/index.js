f4de5a2173954dc89f0d92d39f8e53fce027dca7
Marty-Scott Sainty <saintm02@mc-n356457.local>
2020-04-06 08:06:57 +0100

    Fix merge conflict

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import React from "react";
-<<<<<<< HEAD
 import { StaticRouter } from "react-router-dom";
 import { renderToString } from "react-dom/server";
 import App from "../components/App";
@@ -21,16 +20,6 @@ router.get('/*', async (req, res) => {
     );
 
     res.status(200).render('pages/index', { reactDom });
-=======
-import {renderToString} from "react-dom/server"
-import Index from "../components/pages/index"
-
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    const reactComp = renderToString(<Index/>);
-    res.status(200).render('pages/index', {reactApp: reactComp});
->>>>>>> dc30f591dceae3d7638fbf421dc4c6ed5397f296
 });
 
 export default router;
