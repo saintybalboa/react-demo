672420a47f33d14172fb82b1ad0755131a60e5ef
Marty-Scott Sainty <saintm02@mc-n356457.local>
2020-04-27 12:13:51 +0100

    Add pages

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../../components/Button'
+
+export default function About() {
+    return (
+        <>
+            <h1>About</h1>
+            <Button />
+        </>
+    );
+}
+
