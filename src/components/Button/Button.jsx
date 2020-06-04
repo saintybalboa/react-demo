522c2f7cb4a05d121230ddc21ecec5d691702117
Marty-Scott Sainty <marty-scott.sainty@bbc.co.uk>
2020-06-04 07:35:18 +0100

    Remove source code until tutorial docs have been completed

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Button.scss';
-
-function Button() {
-    return (<button className="rd-button">Test</button>);
-}
-
-export default Button;
--- src/setup-test.js
+++
@@ -1,5 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// Configure Enzyme to work with React
-configure({ adapter: new Adapter() });
